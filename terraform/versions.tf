00f3a12b01306dbffdced1ac60c652a8d1f69dae
Marika Bergman <marika.bergman@gmail.com>
2024-10-18 13:51:27 +0000

    add backend resource details

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,1 +1,8 @@
-terraform {}
+terraform {
+  backend "s3" {
+    bucket = "mariberg-gitops-tf-backend"
+    key    = "terraform.tfstate"
+    region = "eu-west-2"
+    dynamodb_table = "GitopsTerraformLocks"
+  }
+}
