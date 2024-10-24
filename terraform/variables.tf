6b0d372f604d5238c07a792921ba78405f922f97
Marika Bergman <marika.bergman@gmail.com>
2024-10-24 13:16:14 +0000

    add pre-commit hook and fix formatting issues

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,8 @@
 variable "region" {
-    description = "The AWS region to deploy resources in"
-    type        = string
+  description = "The AWS region to deploy resources in"
+  type        = string
 }
 variable "instance_type" {
-    description = "The EC2 instance type"
-    type        = string
+  description = "The EC2 instance type"
+  type        = string
 }
--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mariberg-gitops-tf-backend"
-    key    = "terraform.tfstate"
-    region = "eu-west-2"
+    bucket         = "mariberg-gitops-tf-backend"
+    key            = "terraform.tfstate"
+    region         = "eu-west-2"
     dynamodb_table = "GitopsTerraformLocks"
   }
 }
