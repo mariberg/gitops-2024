c806bc4f95bd364eb49ff179139e592ec4cbaf51
Marika Bergman <marika.bergman@gmail.com>
2024-10-24 12:27:18 +0000

    add missing types

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,2 +1,8 @@
-variable "region" {}
-variable "instance_type" {}
+variable "region" {
+    description = "The AWS region to deploy resources in"
+    type        = string
+}
+variable "instance_type" {
+    description = "The EC2 instance type"
+    type        = string
+}
